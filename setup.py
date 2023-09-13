eec902a54065eba4eff1c64070675ab975c47272
DESKTOP-OHF49GC\anial <aniruddha.a@caprium.com>
2023-09-13 08:21:28 +0530

    correction in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "ext-Summarization-Project"
+REPO_NAME = "Text-Summarization-Project"
 AUTHOR_USER_NAME = "aniruddha26"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "anialkari96@gmail.com"
